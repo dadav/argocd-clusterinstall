b702f67e1147f1f442cf339d335a8cf56a5b20ef
dadav <33197631+dadav@users.noreply.github.com>
2022-08-05 21:05:56 +0200

    feat: use gum

---
+++ README.md
@@ -0,0 +1,9 @@
+# usage
+
+```bash
+# start a k3d cluster
+./setup-cluster.sh
+
+# install the argocd apps
+kubectl create -k argocd-resources
+```
--- setup-cluster.sh
+++ setup-cluster.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
-ns=argocd
+ns="argocd"
+port="8080"
 
-k3d cluster create argocd --api-port 6443 -p 8080:80@loadbalancer --agents 2
+if command -v gum &>/dev/null; then
+  # use fancy gum
+  old_ns="$ns"
+  ns="$(gum input --prompt "Namespace> " --placeholder "What should argocd's namespace be?" <<<$ns)"
+  if [[ "$ns" != "$old_ns" ]]; then
+    # update the namespace in the kustomize file
+    sed -i "s@\(namespace:\) .*@\1 $ns@g" argocd-resources/kustomize.yaml
+  fi
 
-kubectl create ns $ns
-kubectl -n $ns create -f https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml
-kubectl -n $ns patch cm argocd-cmd-params-cm -p '{"data":{"server.insecure":"true"}}'
-kubectl -n $ns patch cm argocd-cmd-params-cm -p '{"data":{"server.rootpath":"/argocd"}}'
-cat <<EOF | kubectl -n $ns create -f -
+  port="$(gum input --prompt "Port> " --placeholder "Which port should be forwarded?" <<<$port)"
+  if ! systemctl is-active docker.service &>/dev/null; then
+    if gum confirm "Should docker be started with sudo now?"; then
+      sudo systemctl start docker.service
+    else
+      exit 1
+    fi
+  fi
+else
+  if ! systemctl is-active docker.service &>/dev/null; then
+    echo "Docker is not running"
+    exit 1
+  fi
+fi
+
+# shellcheck disable=SC2090
+k3d cluster create argocd --api-port 6443 -p "$port":80@loadbalancer --agents 2
+
+kubectl create ns "$ns"
+kubectl -n "$ns" create -f https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml
+kubectl -n "$ns" patch cm argocd-cmd-params-cm -p '{"data":{"server.insecure":"true"}}'
+kubectl -n "$ns" patch cm argocd-cmd-params-cm -p '{"data":{"server.rootpath":"/argocd"}}'
+cat <<EOF | kubectl -n "$ns" create -f -
 ---
 apiVersion: networking.k8s.io/v1
 kind: Ingress
@@ -30,9 +56,10 @@ spec:
 EOF
 
 echo "Waiting for pods to be ready..."
-kubectl -n $ns wait --for=condition=Ready pods --all
+# shellcheck disable=SC2090
+kubectl -n "$ns" wait --for=condition=Ready pods --all
 
-echo "open: http://localhost:8080/argocd/"
+echo "open: http://localhost:$port/argocd/"
 echo "username: admin"
 echo -n "password: "
-kubectl -n $ns get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
+kubectl -n "$ns" get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
